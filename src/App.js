7858519b1c66843d74d085415dfe38947f74f288
Thiem <thiem.huynh0@gmail.com>
2023-03-16 17:06:21 -0400

    adding read receipt icons

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
         <ChatEngine 
             height="100vh"
             projectID="9795d9a8-ba5f-45af-8e1c-424fcb7f0a37"
-            userName="JohnWicky"
+            userName="JoshPecks"
             userSecret="12341234"
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,14 +1,14 @@
 const TheirMessage = ({ lastMessage, message }) =>{
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username != message.sender.username
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
         <div className="message-row">
             {isFirstMessageByUser && (
                 <div 
                     className="message-avatar"
-                    style={{backgroundImage: `url(${message?.sender?.avatar})`}}
+                    style={{backgroundImage: message.sender && `url(${message?.sender?.avatar})`}}
                 />
             )}
-            {message?.attachments?.length > 0
+            {message.attachments && message?.attachments?.length > 0
             ? (
                 <img
                     src={message.attachments[0].file}
@@ -16,14 +16,14 @@ const TheirMessage = ({ lastMessage, message }) =>{
                     className="message-image"
                     style={{ marginLeft : isFirstMessageByUser ? '4px' : '48px' }}
                 />
-            ) : (
+            ) 
+            : (
                 <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft : isFirstMessageByUser ? '4px' : '48px' }}>
                     {message.text}
                 </div>
-            )
-        }
+            )}
         </div>
     );
-}
+};
 
 export default TheirMessage;
